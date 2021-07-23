cde3f6d7b01452a39843de013ce80f3ec1cd882a
Md Parvez Hossain <80127536+mdparvezhossain@users.noreply.github.com>
2021-07-23 22:19:14 +0600

    list modify

--- src/components/formFill/List.jsx
+++ src/components/formFill/List.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 
 class List extends Component {
 
+    data=(dataChild)=>{
+        return <li>{dataChild}</li>
+    }
+
     render() {
         const Name = ['Parvez', 'Priya', 'Roton', 'Riya']
-        const myName =  Name.map((dataChild)=>{
-            return <li>{dataChild}</li>
-        })
+        const myName =  Name.map(this.data)
 
         return (
             <div>
