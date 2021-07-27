37ba6ff4e0428ce93583af20ec185f1935d7df3a
Md Parvez Hossain <80127536+mdparvezhossain@users.noreply.github.com>
2021-07-27 14:14:25 +0600

    Routing is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React  from 'react';
-import CountryList from './components/List/getList';
+
 
 const App = () => {
 
     return (
         <div>
-           <CountryList />
+           This is Home Page
         </div>
     )
 };
--- src/index.js
+++ src/index.js
@@ -2,34 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Header from './Pages/Header';
+import Footer from './Pages/Footer';
+import Portfolio from './Pages/Portfolio';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Route, Link, BrowserRouter as Router } from '../node_modules/react-router-dom';
+import { Route, Link, BrowserRouter as Router } from '../node_modules/react-router-dom';
 
-// const myPages=(
-//     <Router>
-//         <div id="pages-style">
-//            <nav>
-//               <ul>
-//                <Link className="link-tag" to="/">Home</Link>
-//                <Link className="link-tag" to="/header">Header</Link>
-//                <Link className="link-tag" to="/portfolio">Portfolio</Link>
-//                <Link className="link-tag" to="/contact">Contact</Link>
-//                <Link className="link-tag" to="/about">About</Link>
-//                <Link className="link-tag" to="/footer">Footer</Link>
-//               </ul>
-//            </nav>
+const myPages=(
+    <Router>
+        <div id="pages-style">
+           <nav>
+              <ul>
+               <Link className="link-tag" to="/">Home</Link>
+               <Link className="link-tag" to="/header">Header</Link>
+               <Link className="link-tag" to="/portfolio">Portfolio</Link>
+               <Link className="link-tag" to="/contact">Contact</Link>
+               <Link className="link-tag" to="/about">About</Link>
+               <Link className="link-tag" to="/footer">Footer</Link>
+              </ul>
+           </nav>
 
-//             <Route exact path="/" component={App} />
-//             <Route path="/header" component={Header} />
-//             <Route path="/portfolio" component={Portfolio} />
-//             <Route path="/contact" component={Contact} />
-//             <Route path="/about" component={About} />
-//             <Route path="/footer" component={Footer} />
+            <Route exact path="/" component={App} />
+            <Route path="/header" component={Header} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/about" component={About} />
+            <Route path="/footer" component={Footer} />
 
-//         </div>
-//     </Router>
-// )
-ReactDOM.render(
-<> 
-<App />
-</>,document.getElementById('root'));
+        </div>
+    </Router>
+)
+ReactDOM.render(myPages,document.getElementById('root'));
